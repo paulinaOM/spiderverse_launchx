53a85dcc57d93951469ad0279e3ef68aa6a2e172
paulinaOM <17030622@itcelaya.edu.mx>
2022-04-19 20:46:45 -0500

    3. Unit test for spiderman class

---
+++ test/spiderman.test.js
@@ -0,0 +1,10 @@
+describe("Unit Test for Spiderman Class", () => {
+    test('1) Create an spiderman object', () => {
+      const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+      expect(andrewGarfield.name).toBe("Spiderman Sony");
+      expect(andrewGarfield.age).toBe(31);
+      expect(andrewGarfield.actor).toBe("Andrew Garfield");
+      expect(andrewGarfield.movies).toBe(2);
+      expect(andrewGarfield.studio).toBe("Sony");
+    });
+  })
